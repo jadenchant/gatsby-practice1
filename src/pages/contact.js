3961bf90598cd52e822a08528d2c507ca813f70f
Jaden Chant <chant1@stolaf.edu>
2021-02-18 15:03:15 -0600

    bug fix on contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,9 +4,12 @@ import Navbar from "../components/Navbar";
 import "../css/contact.css";
 
 export default function Contact() {
+  // Form State
   const [fname, setFname] = useState("");
   const [lname, setLname] = useState("");
   const [email, setEmail] = useState("");
+
+  // Missing State
   const [missingFname, setMissingFname] = useState("");
   const [missingLname, setMissingLname] = useState("");
   const [missingEmail, setMissingEmail] = useState("");
@@ -41,24 +44,32 @@ export default function Contact() {
     if (fname === "") {
       setMissingFname("Missing First Name");
       missing = true;
+    } else {
+      setMissingFname("");
     }
     if (lname === "") {
       setMissingLname("Missing Last Name");
       missing = true;
+    } else {
+      setMissingLname("");
     }
     if (email === "") {
-      setMissingEmail("Email Address");
-      setIncorrectEmail("Missing @ and . in Email Address");
-      missing = true;
-    } else if (email.indexOf("@") === -1 && email.indexOf(".") === -1) {
+      setMissingEmail("Missing Email Address");
       setIncorrectEmail('Missing "@" and "." in Email Address');
       missing = true;
-    } else if (email.indexOf("@") === -1) {
-      setIncorrectEmail('Missing "@" in Email Address');
-      missing = true;
-    } else if (email.indexOf(".") === -1) {
-      setIncorrectEmail('Missing "." in Email Address');
-      missing = true;
+    } else {
+      setMissingEmail("");
+
+      if (email.indexOf("@") === -1 && email.indexOf(".") === -1) {
+        setIncorrectEmail('Missing "@" and "." in Email Address');
+        missing = true;
+      } else if (email.indexOf("@") === -1) {
+        setIncorrectEmail('Missing "@" in Email Address');
+        missing = true;
+      } else if (email.indexOf(".") === -1) {
+        setIncorrectEmail('Missing "." in Email Address');
+        missing = true;
+      }
     }
 
     return missing;
@@ -70,35 +81,39 @@ export default function Contact() {
       <h1>Contact</h1>
 
       <form>
-        <label>
-          First Name:
+        <label className='form-item'>
+          <p className='form-item-name'>First Name *</p>
           <input
             name='fname'
             type='string'
             value={fname}
             onChange={handleInputChange}
+            className='form-item-input'
           />
         </label>
-        <label>
-          Last Name:
+        <label className='form-item'>
+          <p className='form-item-name'>Last Name *</p>
           <input
             name='lname'
             type='string'
             value={lname}
             onChange={handleInputChange}
+            className='form-item-input'
           />
         </label>
-        <label>
-          Email:
+        <label className='form-item'>
+          <p className='form-item-name'>Email: *</p>
           <input
             name='email'
             type='string'
             value={email}
             onChange={handleInputChange}
+            className='form-item-input'
           />
         </label>
-        <button onClick={handleSubmit}>Submit</button>
-        <div className='form-error'></div>
+        <button onClick={handleSubmit} className='form-submit'>
+          Submit
+        </button>
       </form>
 
       <div className='input-error'>
