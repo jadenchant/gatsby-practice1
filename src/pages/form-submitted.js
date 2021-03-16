fbf8c93bb9251b656737e136bb85c45663700df1
Jaden Chant <chant1@stolaf.edu>
2021-03-15 20:48:55 -0500

    updated the contact form scss

--- src/pages/form-submitted.js
+++ src/pages/form-submitted.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { navigate } from "gatsby";
+import React from "react";
+// import { navigate } from "gatsby";
 import Navbar from "../components/Navbar";
 import "../css/contact.css";
 
